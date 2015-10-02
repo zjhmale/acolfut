6a71a676b4166fe7b5b463d0ae5d83d4610d35fd
zjhmale <zjhmale@gmail.com>
2015-10-03 01:34:03 +0800

    update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject acolfut "0.1.0"
   :description "a coloful test framework for clojure inspired by clojure.test"
+  :url "https://github.com/zjhmale/acolfut"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
